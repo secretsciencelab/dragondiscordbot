00a0096843a704115d28014d92d2b3645cd783f1
Aaron <aaron@secretsciencelab.com>
2018-09-25 20:58:42 -0700

    Added Discord piece

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+import os
+
+TOKEN = os.environ["DISCORD_TOKEN"]
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
