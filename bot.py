3d64f2c4f73bbbbbb9be808b532a83ea77457f6d
nS-Kami64 <43598981+nS-Kami64@users.noreply.github.com>
2018-09-26 05:26:24 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,10 +66,12 @@ async def chris():
 @bot.command()
 async def kami():
   await bot.say("He's not a god!")
+
 @bot.command()
-async def cade()
+async def cade():
   await bot.say("He's Helpful and Kind")
-@bot.command()
+
+    @bot.command()
 async def aboutds():
   embed=discord.Embed(title="About Dragonscript Arena", description="Dragonscript arena is a game designed to help it's players learn JavaScript while controlling/programming an AI for their dragons to go into battle!", color=0x1abc9c)
   embed.set_thumbnail(url="https://i.imgur.com/6QgGoAq.png")
