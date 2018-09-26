6b6f2fb48185773b2d6035e386ccfdc286bc2974
Cade Rinker <43603360+DoctorMuncher@users.noreply.github.com>
2018-09-26 07:26:20 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,16 +80,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('!Goodnight'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-@client.event
-async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
 
 client.run('token')
 
