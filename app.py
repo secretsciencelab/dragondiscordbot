e506335def6c4073ab346272c41d9b377061aef4
Aaron <aaron@secretsciencelab.com>
2018-09-25 22:04:28 -0700

    Bind to 0.0.0.0

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def homepage():
 
 def startFlask():
   port = int(os.environ.get("PORT", 8000))
-  app.run(debug=True, use_reloader=False, port=port)
+  print(port)
+  app.run(debug=True, use_reloader=False, host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     flaskThread = threading.Thread(target=startFlask)
