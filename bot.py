799fe1c5d1613c8f114b429bad3d202efb905132
Aaron <aaron@secretsciencelab.com>
2018-09-25 22:44:01 -0700

    8ball

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import discord
 import os
+from discord import Game
+from discord import Bot
 
 TOKEN = os.environ["DISCORD_TOKEN"]
 
-client = discord.Client()
+BOT_PREFIX = ("$", "!")
+client = Bot(command_prefix=BOT_PREFIX)
 
 @client.event
 async def on_message(message):
@@ -21,6 +24,21 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
 
 def startDiscord():
   client.run(TOKEN)
