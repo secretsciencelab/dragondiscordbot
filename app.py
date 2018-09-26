d6c12b8d78c219e555cc62d90a79367b30385a40
Aaron <aaron@secretsciencelab.com>
2018-09-25 21:06:37 -0700

    Check deploy

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from datetime import datetime
+from bot import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    token = os.environ["DISCORD_TOKEN"]
 
     return """
     <h1>Dragon's Blessings!</h1>
     <p>I am alive at {time}.</p>
+    <p>{token}.</p>
     """.format(time=the_time)
 
 if __name__ == '__main__':
