cd4ce8e2652655f554a841205fe2fced795fdb2f
Aaron <aaron@secretsciencelab.com>
2018-09-25 20:26:05 -0700

    Add basic Flask server

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Dragon's Blessings!</h1>
+    <p>I am alive at {time}.</p>
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
