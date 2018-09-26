5ce129609f670513af0ca0ba00d24a65b0d932ab
DoctorMuncher <43603360+DoctorMuncher@users.noreply.github.com>
2018-09-26 05:25:09 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -68,7 +68,7 @@ async def kami():
   await bot.say("He's not a god!")
 @bot.command()
 async def cade()
-await bot.say("He's Helpful and Kind")
+  await bot.say("He's Helpful and Kind")
 @bot.command()
 async def aboutds():
   embed=discord.Embed(title="About Dragonscript Arena", description="Dragonscript arena is a game designed to help it's players learn JavaScript while controlling/programming an AI for their dragons to go into battle!", color=0x1abc9c)
