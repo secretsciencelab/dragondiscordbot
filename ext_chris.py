815bfbd8ed2c706c797f4f0f5eb99432a111090e
Steel_Dev <reals7eel02@gmail.com>
2018-09-26 23:47:05 -0400

    f

--- ext_chris.py
+++ ext_chris.py
@@ -5,13 +5,13 @@ class Chris():
   def __init__(self, bot):
     self.bot = bot
 
-@commands.command()
-async def summonmaster(self):
-    await self.bot.say('mmmm~ you summoned the dungeon master~ heheheh... <@214472130627239946>')
+  @commands.command()
+  async def summonmaster(self):
+     await self.bot.say('mmmm~ you summoned the dungeon master~ heheheh... <@214472130627239946>')
 
-@commands.command()
-async def ping(self):
-    await self.bot.say('PONG BITCH!')
+  @commands.command()
+  async def ping(self):
+     await self.bot.say('PONG BITCH!')
 
 def setup(bot):
   bot.add_cog(Chris(bot))
