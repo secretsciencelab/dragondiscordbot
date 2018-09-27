c7936e61b83035f7e45e538bbff746391306d0fa
Steel_Dev <reals7eel02@gmail.com>
2018-09-26 23:14:50 -0400

    chris ext

---
+++ ext_chris.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class Chris():
+  def __init__(self, bot):
+    self.bot = bot
+
+  @commands.command()
+  async def ping(self):
+    await self.bot.say('PONG BITCH!')
+
+def setup(bot):
+  bot.add_cog(Chris(bot))
