fd5c5255ac85ed50a3803f6fde5b33487510211b
Steel_Dev <reals7eel02@gmail.com>
2018-09-27 02:19:03 -0400

    Updated my ext

--- ext_chris.py
+++ ext_chris.py
@@ -17,5 +17,16 @@ class Chris():
   async def crazy(self):
     await self.bot.say('oh no~ theres nothing you can throw at me.. *you cant win against my kind of crazy~*')
 
+  @commands.command()
+  async def snap(self):
+    possible_responses = [
+      'JIMMY! IMMA GET THE MOB BOSS ON YOUR ASS!',
+      'MELVIN! IMMA HACK YOUR ROUTER, BISH!',
+      'JERRY! IMMA STUFF YO ASS WITH BEEEANNNSSS, YEAH',
+      'JUDY! NOW GIVE ME MY FUKIN NEWPORTS BITCH!',
+      'ALISTAIR! Get in the fucking Dungeon and prepare for the most pain you have ever felt~',
+    ]
+    await self.bot.say('hmm.. who shall I be today? *snaps fingers* oh.. now im ' + random.choice(possible_responses))
+
 def setup(bot):
   bot.add_cog(Chris(bot))
