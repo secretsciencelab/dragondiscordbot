794a28c671fe0d7da742fc6bf6e2f8e246dbcbe4
Steel_Dev <reals7eel02@gmail.com>
2018-09-26 23:32:36 -0400

    ff

--- ext_chris.py
+++ ext_chris.py
@@ -4,10 +4,13 @@ from discord.ext import commands
 class Chris():
   def __init__(self, bot):
     self.bot = bot
-    self.bot.say('mmmm~ you summoned the dungeon master~ heheheh...')
 
-  @commands.command()
-  async def ping(self):
+@commands.command()
+async def summon(self):
+   await self.bot.say('mmmm~ you summoned the dungeon master~ heheheh... ' + <@214472130627239946>)
+
+@commands.command()
+async def ping(self):
     await self.bot.say('PONG BITCH!')
 
 def setup(bot):
