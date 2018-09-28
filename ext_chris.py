168bd6c20e837195e4aa54368ba89a183378ed84
Steel_Dev <reals7eel02@gmail.com>
2018-09-27 23:12:52 -0400

    fds

--- ext_chris.py
+++ ext_chris.py
@@ -145,10 +145,10 @@ class Chris():
         return   
 
       if lastuserstolenfrom == None:
-        botdb.set(laststolenfromkey, {'lastuserstolenfrom': "none"}, "stealing")
+        botdb.set(laststolenfromkey, {'lastuserstolen': 'none'}, "stealing")
         lastuserstolenfrom = botdb.get(laststolenfromkey, "stealing")
 
-      if target.name == lastuserstolenfrom['lastuserstolenfrom'] and lastuserstolenfrom != None:
+      if target.name == lastuserstolenfrom['lastuserstolen'] and lastuserstolenfrom != None:
         eremb=discord.Embed(title="Stealing [ERROR]", description="This user has already been stolen from last. try someone else.", color=0xFF0000)
         await self.bot.say(context.message.author.mention, embed=eremb)
         return
