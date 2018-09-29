ac8da258622efc1cc333f69b476e93dfff07ed6a
Cade Rinker <43603360+DoctorMuncher@users.noreply.github.com>
2018-09-28 23:10:19 -0400

    Update ext_cade.py

--- ext_cade.py
+++ ext_cade.py
@@ -21,6 +21,30 @@ class Cade():
       await self.bot.say("%s" % doc['value'])
     else:
       await self.bot.say("Not found")
+      
+def hello_function(message,client,args):
+    try:
+        return 'Hello {}, Argument One: {}'.format(message.author, args[0])
+    except Exception as e:
+        return e
+
+# hello command dictionary
+hello_command = {
+    # if the string starts with this the function will be called
+    'trigger': '!hello',
+    
+    # specifies the function to call
+    'function': hello_function,
+    
+    # number of arguments that the functions needs
+    'args_num': 1,
+    
+    # name of the argument that the funtions takes
+    'args_name': ['string'],
+    
+    # describes what the function does
+    'description': 'Will respond hello to the caller and show arg 1'
+}
 
 def setup(bot):
   bot.add_cog(Cade(bot))
