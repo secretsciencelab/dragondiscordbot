66fd8915beb553024324a0b690f24e1429b362c0
Cade Rinker <43603360+DoctorMuncher@users.noreply.github.com>
2018-10-04 09:05:44 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -133,6 +133,10 @@ async def kami():
   await bot.say("He's not a god!")
 
 @bot.command()
+async def Steven():
+  await bot.say("Ya Yeet, mofos. BTW LIFE SUCKS ASS")
+
+@bot.command()
 async def cade():
   await bot.say("He's a very happy guy with his man's Alyx c:")
 
