8bfcb015584d22482fe9dbeff67c57a3771664da
nS-Kami64 <43598981+nS-Kami64@users.noreply.github.com>
2020-01-08 23:58:10 -0400

    Update ext_kami.py

--- ext_kami.py
+++ ext_kami.py
@@ -1,17 +1,17 @@
 import discord
 from discord.ext import commands
 
-class kami():
+class Kami():
   def __init__(self, bot):
     self.bot = bot
 
   @commands.command()
-  async def owo():
-    await self.bot.say("What's this?")
+    async def owo():
+      await self.bot.say("What's this?")
 
   @commands.command()
-  asyn def joker():
-    await self.bot.say("Looking cool Joker!")
+    async def joker():
+      await self.bot.say("Looking cool Joker!")
 
 def setup(bot):
   bot.add_cog(Kami(bot))
