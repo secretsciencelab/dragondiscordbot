c66b013c57c25bed4a007fd6b05e472448562e00
Dragondrawer <59541838+Dragondrawer@users.noreply.github.com>
2020-01-11 10:58:28 -0600

    Update ext_dragondrawer.py

--- ext_dragondrawer.py
+++ ext_dragondrawer.py
@@ -9,6 +9,10 @@ class Dragondrawer():
   @commands.command()
   async def Drawadragon(self):
     await self.bot.say("https://i.ytimg.com/vi/Iqr_yP25wug/maxresdefault.jpg")
+    
+  @commands.command()
+  async def babyyoda(self):
+    await self.bot.say("A wholesome meme...https://media1.tenor.com/images/42ee9b64cad9d525e2fcea4530b4200f/tenor.gif?itemid=15914167")
 
 def setup(bot):
   bot.add_cog(Dragondrawer(bot))
