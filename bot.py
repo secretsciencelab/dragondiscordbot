9b248b563467244e116b8caa10a5ede37ce0599f
Dragondrawer <59541838+Dragondrawer@users.noreply.github.com>
2020-01-13 17:22:30 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -190,7 +190,7 @@ async def rawr():
     
 @bot.command()
 async def boat():
-    await bot.say("i could play?")
+    await bot.say("hi @Aaron | DragonScript Arena!")
     
 @bot.command()
 async def Dragondrawer():
