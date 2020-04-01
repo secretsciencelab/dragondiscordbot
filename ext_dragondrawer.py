48e703eff976ad59f3da54e14d05268a254330a8
Dragondrawer <59541838+Dragondrawer@users.noreply.github.com>
2020-03-31 20:15:33 -0700

    Update ext_dragondrawer.py

--- ext_dragondrawer.py
+++ ext_dragondrawer.py
@@ -11,16 +11,16 @@ class Dragondrawer():
     self.bot = bot
 
   @commands.command()
-  async def Drawadragon(self):
-    await self.ctx.send("https://i.ytimg.com/vi/Iqr_yP25wug/maxresdefault.jpg")
+  async def drawadragon(self, ctx):
+    await ctx.send('https://i.ytimg.com/vi/Iqr_yP25wug/maxresdefault.jpg') 
     
   @commands.command()
-  async def babyyoda(self):
-    await self.ctx.send("A wholesome meme...https://media1.tenor.com/images/42ee9b64cad9d525e2fcea4530b4200f/tenor.gif?itemid=15914167")
+  async def babyyoda(self, ctx):
+    await ctx.send('A wholesome meme...https://media1.tenor.com/images/42ee9b64cad9d525e2fcea4530b4200f/tenor.gif?itemid=15914167')
     
   @commands.command()
-  async def pKIRBY(self):
-    await self.ctx.send("Desc: my character is per say 5 foot 10 and is a Male https://cdn.discordapp.com/attachments/691091650797240360/694733357551976488/sshot-049.png")
+  async def pKIRBY(self, ctx):
+    await ctx.send("Desc: NONE https://cdn.discordapp.com/attachments/691091650797240360/694733357551976488/sshot-049.png")
 
 def setup(bot):
   bot.remove_command("Drawadragon")
