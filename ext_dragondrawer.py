95308a6eb5f3168e249121151d73a3080f7eb747
Dragondrawer <59541838+Dragondrawer@users.noreply.github.com>
2020-03-31 19:23:37 -0700

    Update ext_dragondrawer.py

--- ext_dragondrawer.py
+++ ext_dragondrawer.py
@@ -13,6 +13,11 @@ class Dragondrawer():
   @commands.command()
   async def babyyoda(self):
     await self.ctx.send("A wholesome meme...https://media1.tenor.com/images/42ee9b64cad9d525e2fcea4530b4200f/tenor.gif?itemid=15914167")
+    
+  @commands.command()
+  async def pK I R B Y(self):
+    await self.ctx.send("https://cdn.discordapp.com/attachments/691091650797240360/694733357551976488/sshot-049.png")
 
 def setup(bot):
   bot.add_cog(Dragondrawer(bot))
+https://cdn.discordapp.com/attachments/691091650797240360/694733357551976488/sshot-049.png
