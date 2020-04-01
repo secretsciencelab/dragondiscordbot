080faef69429666b6af4ebe98f7d6fab65823aef
Dragondrawer <59541838+Dragondrawer@users.noreply.github.com>
2020-03-31 19:30:26 -0700

    Update ext_dragondrawer.py

--- ext_dragondrawer.py
+++ ext_dragondrawer.py
@@ -16,7 +16,7 @@ class Dragondrawer():
     
   @commands.command()
   async def pK I R B Y(self):
-    await self.ctx.send("https://cdn.discordapp.com/attachments/691091650797240360/694733357551976488/sshot-049.png")
+    await self.ctx.send("Desc: https://cdn.discordapp.com/attachments/691091650797240360/694733357551976488/sshot-049.png")
 
 def setup(bot):
   bot.add_cog(Dragondrawer(bot))
